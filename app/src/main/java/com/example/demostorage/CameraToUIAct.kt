c44944a305bc0f4ac7920be92e1265c7b81f0e90
dell <hieppdmascom@gmail.com>
2022-11-09 17:46:53 +0700

    save image in ExternalStorage

--- app/src/main/java/com/example/demostorage/CameraToUIAct.kt
+++ app/src/main/java/com/example/demostorage/CameraToUIAct.kt
@@ -1,9 +1,11 @@
 package com.example.demostorage
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
@@ -33,7 +35,38 @@ class CameraToUIAct: AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             val bitmap: Bitmap = BitmapFactory.decodeFile(pathToFile)
             imvCameraToUI.setImageBitmap(bitmap)
+            savePhotoToExternalStorage(pathToFile!!,bitmap)
         }
+
+    private fun savePhotoToExternalStorage(name: String, bitmap: Bitmap): Boolean {
+        // dir
+        val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        else MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        // metadata
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "$name.jpg")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.WIDTH, bitmap.width)
+            put(MediaStore.Images.Media.HEIGHT, bitmap.height)
+        }
+
+        // save file
+        return try {
+            contentResolver.insert(imageCollection, contentValues)?.also { uri ->
+                contentResolver.openOutputStream(uri).use { outputStream ->
+                    if (!bitmap.compress(Bitmap.CompressFormat.JPEG, 95, outputStream)) {
+                        throw IOException("Couldn't save bitmap")
+                    }
+                }
+            } ?: throw IOException("Couldn't create MediaStore entry")
+
+            true
+        } catch (e: IOException) {
+            println("debug: ${e.message}")
+            false
+        }
+    }
 
     @SuppressLint("QueryPermissionsNeeded")
     private fun captureImageFromCamera() {
